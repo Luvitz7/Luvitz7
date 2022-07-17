4a0e7ce95fdcbef1a621959bebcb8231a0bb70d6
ludwigj <lu.pv7@outlook.com>
2022-07-17 17:07:33 -0500

    sin animacion de skills

--- src/script.js
+++ src/script.js
@@ -51,20 +51,20 @@ eyesTimer();
 
 // Scroll anitions page
 
-const containerSkillsLeft = document.getElementById('container-Skills-Left');
-const containerSkillsRight = document.getElementById('container-Skills-Right');
+// const containerSkillsLeft = document.getElementById('container-Skills-Left');
+// const containerSkillsRight = document.getElementById('container-Skills-Right');
 
-window.addEventListener('scroll', () => {
-    const positionOBJ = containerSkillsLeft.getBoundingClientRect().top;
-    console.log(positionOBJ);
-    if (positionOBJ <= 450 ) {
-        containerSkillsLeft.style.animation = 'skillsMoveLeft ease-in-out 2s'
-        containerSkillsRight.style.animation = 'skillsMoveRight ease-in-out 2s'
-    }
+// window.addEventListener('scroll', () => {
+//     const positionOBJ = containerSkillsLeft.getBoundingClientRect().top;
+//     console.log(positionOBJ);
+//     if (positionOBJ <= 450 ) {
+//         containerSkillsLeft.style.animation = 'skillsMoveLeft ease-in-out 2s'
+//         containerSkillsRight.style.animation = 'skillsMoveRight ease-in-out 2s'
+//     }
 
-})
+// })
 
-const skillSection = window.location.hash
-console.log(skillSection);
+// const skillSection = window.location.hash
+// console.log(skillSection);
 
 // Porfolio cicle
